a181f694c73cdcef9fb334629edd1b9c83065ac0
bagmanciabdullah <bagmanciabdullah93@gmail.com>
2026-09-05 00:17:56 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,4 @@
-// Supabase bilgilerini buraya yazacaksin.
-// Supabase panelinde: Project Settings > API
 window.BAGMANCI_SUPABASE = {
-  url: '',https://gckwzfxypovynbwoceey.supabase.co/rest/v1/
-  anonKey: ''sb_publishable_u-7LFFT1nupBcvI14pyZSA_ksfnKwQM
+  url: 'https://gckwzfxypovynbwoceey.supabase.co',
+  anonKey: 'sb_publishable_u-7LFFT1nupBcvI14pyZSA_ksfnKwQM'
 };
